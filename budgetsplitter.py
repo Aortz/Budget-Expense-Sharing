a1deff381e94bdbb18ccdd20b864279b4e6e5bd1
Aortz <65661817+Aortz@users.noreply.github.com>
2020-12-24 21:45:05 +0800

    Update budgetsplitter.py

--- budgetsplitter.py
+++ budgetsplitter.py
@@ -1,36 +1,39 @@
+import pandas as pd
+
+no_of_pax = 6
+total_amount = 138
+paid_py_person = [47,37,24,0,30,0]
+namelist = ["Pek","Shilin","Junwei","Matt","Clement","Chin Wei"]
+
+#creating fuction to calculate the difference in amount paid by specific person and average amount per pax
 def per_pax_payment(paid_py_person, no_of_pax, total_amount):
 	averageAmount = paymentcalc(no_of_pax, total_amount)
 	amount_owed_by_thisguy = averageAmount - paid_py_person
 	return (round(amount_owed_by_thisguy, 2))
 
-
+#function to calculate average cost of the bill
 def paymentcalc(no_of_pax, total_amount):
 	amount_to_pay_per_pax = total_amount/no_of_pax
 	return(round(amount_to_pay_per_pax, 2))
 
+#function to calculate difference between those owing money and those who needs to be paid
 def getDiff(x,y):
-	if x < y:
-		difference = x + y
-	elif y < x:
-		difference = y
+	difference = x + y
 	return difference
 
 #Payment by person in a list form in descending order
 
-no_of_pax = 3
-total_amount = 270.00
-paid_py_person = [97, 82, 91]
 
 #Creating a drop down option, where user inputs amounts paid in descending order
 #Creation of input values to replace hard coded elements in namelist, for user interface
-namelist = ["Justin", "Junwei", "Adrian"]
 
-#create dictionary for amount everyone paid
+
+#create dictionary for difference in amount that everyone paid
 my_dict = {}
 def makeDict(namelist, paid_py_person):
-	count = 1
-	while count < no_of_pax + 1:
-		my_dict[namelist[count - 1]] = per_pax_payment(paid_py_person[count - 1], no_of_pax, total_amount)
+	count = 0
+	while count < no_of_pax:
+		my_dict[namelist[count - 0]] = per_pax_payment(paid_py_person[count], no_of_pax, total_amount)
 		count += 1
 	return my_dict
 
@@ -40,9 +43,9 @@ makeDict(namelist, paid_py_person)
 #create two list with amounts outstanding and amounts due
 dict_for_ppl_owing_money = {}
 dict_for_ppl_owed_money = {}
-total_amount_oustanding = []
+total_amount_oustanding = [] #creating a counter to keep track of total sum outstanding after deductions
 
-for name, value in my_dict.items():
+for name, value in my_dict.items(): #sorting keys and values into those who paid and those who didnt
 	if value < 0:
 		dict_for_ppl_owed_money[name] = float(value)
 		namelist_of_those_owed_money = list(dict_for_ppl_owed_money.keys())
@@ -53,85 +56,51 @@ for name, value in my_dict.items():
 		amount_of_those_owing_money = list(dict_for_ppl_owing_money.values())
 		namelist_of_those_owing_money = list(dict_for_ppl_owing_money.keys())
 
+
 for values in amount_owed:
 	total_amount_oustanding.append(values * -1)
 
 total_amount_oustanding = sum(total_amount_oustanding)
 
+payment_dict = {}
+
 #creating an algorithm to determine distribution of funds
 #compare the difference in amount first person havent paid and first person paid
 
-payor = 0
-difference = 0
+payor = 0 #initialise value to first element in the dictionaries
+difference = 0 #initialising first value of difference
 
-while total_amount_oustanding > 0:
-	name_for_currentperson_owing_money = namelist_of_those_owing_money[payor]
+while total_amount_oustanding > 0: #creating a sentinel value to stop the programme when outstanding <0
+
 	try:
-		whotowho = str(name_for_currentperson_owing_money) + " owes " + str(namelist_of_those_owed_money[payor])
-	except IndexError:
+		name_for_currentperson_owing_money = namelist_of_those_owing_money[0]
+		#defining key for name in dict for those who owe money against first name in those that didn't 
+		whotowho = f"{name_for_currentperson_owing_money} owes {namelist_of_those_owed_money[payor]}" 
+	except IndexError: #error handling for the case where they delete the first element in the dictionary and list when there is nothing left to compare to
 		break
 	finally:
 		pass
 	
-	difference = round(getDiff(amount_owed[payor], amount_of_those_owing_money[payor]), 2)
+	actual_difference = round(getDiff(amount_owed[payor], amount_of_those_owing_money[payor]), 2)
 
-	if difference > 0:
-		dict_for_ppl_owing_money[whotowho] = amount_owed[payor] * -1
-		total_amount_oustanding += amount_owed[payor]
+	if actual_difference > 0: #aka those owing money paid amount > the first person who's owed money
+		#value is negative so *-1 to make it a positive amount and amount owed is the entire sum of the first person paid money
+		payment_dict[whotowho] = amount_owed[payor] * -1
+		total_amount_oustanding += amount_owed[payor] #updating counter
+		#after deduction is done, update the dictionary to remove the first person paid
+		amount_of_those_owing_money[payor] = actual_difference
+		dict_for_ppl_owing_money[name_for_currentperson_owing_money] = actual_difference
 		del amount_owed[payor]
 		del dict_for_ppl_owed_money[namelist_of_those_owed_money[payor]]
 		del namelist_of_those_owed_money[payor]
 
-	elif difference <= 0:
-		dict_for_ppl_owing_money[whotowho] = amount_of_those_owing_money[payor]
+	elif actual_difference <= 0:
+		payment_dict[whotowho] = amount_of_those_owing_money[payor]
 		total_amount_oustanding -= amount_of_those_owing_money[payor]
 		del dict_for_ppl_owing_money[name_for_currentperson_owing_money]
-		dict_for_ppl_owed_money[namelist_of_those_owed_money[payor]] = difference
-		amount_owed[payor] = difference
+		dict_for_ppl_owed_money[namelist_of_those_owed_money[payor]] = actual_difference
+		amount_owed[payor] = actual_difference
 		del amount_of_those_owing_money[payor]
 		del namelist_of_those_owing_money[payor]
 
-print(dict_for_ppl_owing_money)
-
-#creation of error handling method in case of mismatched between total amount inputted and total amount paid by individual users
-#error handling for when number of elements in a namelist doesn't match number of elements for amounts paid
-
-
-# for name, payee in dict_for_ppl_owed_money.items():
-# 	#creating new key in outstanding dict for who paid who
-# 	name_for_currentperson_owing_money = list(dict_for_ppl_owing_money.keys())[payor]
-# 	whotowho = str(name_for_currentperson_owing_money) + " owes " + str(namelist_of_those_owed_money[payor])
-# 	difference = round(getDiff(amount_owed[payor], amount_of_those_owing_money[payor]), 2)
-
-# 	if difference > 0:
-# 		dict_for_ppl_owing_money[whotowho] = amount_owed[payor] * -1
-# 		del amount_owed[payor]
-# 		# del dict_for_ppl_owed_money[namelist_of_those_owed_money[payor]]
-# 		del namelist_of_those_owed_money[payor]
-# 		amount_of_those_owing_money[payor] = difference
-# 		whotowho = str(name_for_currentperson_owing_money) + " owes " + str(name)
-# 		difference = round(getDiff(amount_owed[payor], amount_of_those_owing_money[payor]), 2)
-# 		dict_for_ppl_owing_money[whotowho] = amount_of_those_owing_money[payor] - difference
-
-# 		# difference = round(getDiff(amount_owed[payor], amount_of_those_owing_money[payor]), 2)
-# 		# if difference > 0:
-# 		# 	dict_for_ppl_owing_money[whotowho] = amount_of_those_owing_money[payor] - difference
-# 		# else:
-# 		# 	continue
-
-# 		# del dict_for_ppl_owed_money[name_for_currentperson_owing_money]
-# 		# del namelist_of_those_owing_money[payor]
-
-
-
-
-# 	elif difference <= 0:
-# 		dict_for_ppl_owing_money[whotowho] = amount_of_those_owing_money[payor]
-# 		del dict_for_ppl_owing_money[name_for_currentperson_owing_money]
-# 		dict_for_ppl_owed_money[name] = difference
-# 		del amount_of_those_owing_money[payor]
-# 		amount_owed[payor] = difference
-
-# print(dict_for_ppl_owing_money)
-
-
+print(payment_dict)
